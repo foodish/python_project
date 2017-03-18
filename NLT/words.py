c7869da345bac281249b45926828950eaf766f60
foodish <xbj1900@gmail.com>
2017-03-19 01:00:35 +0800

    add jieba

--- NLT/words.py
+++ NLT/words.py
@@ -6,8 +6,8 @@ import jieba
 text = open('1.txt', encoding='utf-8').read()
 
 wordlist_jieba = jieba.cut(text, cut_all = True)
-wl_space_split = ''.join([i for i in wordlist_jieba])
-#wl_space_split = ''.join(wordlist_jieba)  # 后面报错：IndexError: list index out of range
+# wl_space_split = ' '.join([i for i in wordlist_jieba])
+wl_space_split = ' '.join(wordlist_jieba)  # 注意空格！！否则报错！！后面报错：IndexError: list index out of range
 
 my_wordcloud = WordCloud().generate(wl_space_split)
 
